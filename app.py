c0470a4a4318cc6dde16b4cc3aa4c3c680cd0572
Felix Sargent <felix.sargent@gmail.com>
2013-03-20 19:46:30 -0700

    Why did I remove the bit that made heroku work/?

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def index(name=None):
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 8080.
-    port = 8080
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
